e67d2db0c933b3cfdc588731b7d611496f580a31
Okalll <vivieokall@gmail.com>
2022-12-04 17:29:24 +0000

    Reversed list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+	my_list = []
+	new_list = [my_list[len(my_list) - i]
+		for i in range91, len(my_list)+1)]
+print('{:d}'.format(new_list[i]))
